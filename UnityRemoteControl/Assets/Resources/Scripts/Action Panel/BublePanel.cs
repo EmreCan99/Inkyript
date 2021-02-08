2f15eb7473836e3bb8c9386a8670149df6443407
Emre Can Çiftçi <77917464+EmreCan99@users.noreply.github.com>
2021-02-09 00:26:37 +0300

    Bubble panel merge
    - Animation delay coroutine
    - set parent to actPanel

--- UnityRemoteControl/Assets/Resources/Scripts/Action Panel/BublePanel.cs
+++ UnityRemoteControl/Assets/Resources/Scripts/Action Panel/BublePanel.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class demoPanel : MonoBehaviour
+public class BublePanel : MonoBehaviour
 {
     Animator _animator;
+    [SerializeField] QuotesPanel _quotesPanel;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
     }
-    public void OpenActPanel()
+    public void OpenBubblePanel()
     {
         bool isOpen = _animator.GetBool("isPanelOpen");
         _animator.SetBool("isPanelOpen", !isOpen);
+
+        StartCoroutine(WaitForBubble());
+    }
+
+    IEnumerator WaitForBubble()
+    {
+        yield return new WaitForSeconds(.3f);
+
+        _quotesPanel.OpenActPanel(0);
     }
 }
