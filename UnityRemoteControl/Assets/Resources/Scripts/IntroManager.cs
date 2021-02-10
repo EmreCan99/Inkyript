5dff83565317f6b66cc11da196ed7207df66db5a
Emre Can Çiftçi <77917464+EmreCan99@users.noreply.github.com>
2021-02-10 18:21:08 +0300

    Splash screen and intro animation added

---
+++ UnityRemoteControl/Assets/Resources/Scripts/IntroManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        StartCoroutine(WaitforIntroToFinish());
+    }
+
+    IEnumerator WaitforIntroToFinish()
+    {
+        yield return new WaitForSeconds(3.70f);
+
+        SceneManager.LoadScene(0);
+    }
+}
