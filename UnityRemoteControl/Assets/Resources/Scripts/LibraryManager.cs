c91c6709e88ebfcb046431b2affa98f2d75a8ebc
Emre Can Çiftçi <77917464+EmreCan99@users.noreply.github.com>
2021-02-14 15:41:36 +0300

    Back button function

--- UnityRemoteControl/Assets/Resources/Scripts/LibraryManager.cs
+++ UnityRemoteControl/Assets/Resources/Scripts/LibraryManager.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LibraryManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Back button
+        #region Android
+
+        // Make sure user is on Android platform
+        if (Application.platform == RuntimePlatform.Android)
+        {
+
+            // Check if Back was pressed this frame
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                // Load main menu
+                SceneManager.LoadScene(0);
+            }
+            #endregion
+        }
     }
+
 }
