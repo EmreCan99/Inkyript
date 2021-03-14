c36ec41ba61747dbd2c0fa5f5e989e9267d81e29
Emre Can Çiftçi <77917464+EmreCan99@users.noreply.github.com>
2021-03-14 18:17:13 +0300

    Achievement popup

--- UnityRemoteControl/Assets/Resources/Scripts/GameScene/GameUIManager.cs
+++ UnityRemoteControl/Assets/Resources/Scripts/GameScene/GameUIManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class GameUIManager : MonoBehaviour
 {
     QuoteDB quote;
-    [SerializeField] GameObject sharePanel;
+    [SerializeField] GameObject sharePanel, specialAchPopUp;
     
    
    
@@ -18,9 +18,22 @@ public class GameUIManager : MonoBehaviour
 
     }
 
+    private void Update()
+    {
+    }
+
     public void SaveHistory()
     {
         quote = GameManager.Instance.quote;
+
+        if (quote.category.ToString() == "Author\r")
+        {
+            SpecialAchived(quote, "Author");
+        }
+        else if (quote.category.ToString() == "Book\r")
+        {
+            SpecialAchived(quote, "Book");
+        }
 
         GameManager.Instance.SaveHistory(quote);
 
@@ -51,5 +64,34 @@ public class GameUIManager : MonoBehaviour
     {
         GameManager.Instance.ShowAds();
         GameManager.Instance.NewGame(0);
+    }
+
+    void SpecialAchived(QuoteDB quote, string category)
+    {
+        specialAchPopUp.transform.GetChild(0).GetComponent<Text>().text = "New " + category + " is unlocked";
+        if (category == "Author")
+        {
+            specialAchPopUp.transform.GetChild(1).GetChild(1).GetComponent<Text>().text = quote.author.ToString();
+        }
+        else if (category == "Book")
+        {
+            specialAchPopUp.transform.GetChild(1).GetChild(1).GetComponent<Text>().text = quote.book.ToString();
+        }
+
+
+        specialAchPopUp.SetActive(true);
+        StartCoroutine(ExitAchPopup());
+    }
+
+    IEnumerator ExitAchPopup()
+    {
+        yield return new WaitForSeconds(3f);
+
+        InitExitAchPopup();
+    }
+
+    public void InitExitAchPopup()
+    {
+        specialAchPopUp.GetComponent<Animator>().SetBool("isExit", true);
     }
 }
